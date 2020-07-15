24b1de486d00f40f5157f67282ee6628bb2cfea9
jlugao <joaolhullier@gmail.com>
2020-07-14 23:34:41 -0300

    remove Depends from crud file

--- sucuri/crud/crud_profile.py
+++ sucuri/crud/crud_profile.py
@@ -1,4 +1,3 @@
-from fastapi import Depends
 from sqlalchemy.orm import Session
 
 # IIB
@@ -7,7 +6,7 @@ from sucuri.models.models import Profile
 from sucuri.schemas.schemas import ProfileSchema
 
 
-def post_profile(profile: ProfileSchema, db: Session = Depends(get_db)):
+def post_profile(profile: ProfileSchema, db: Session):
     db_profile = Profile(**profile.dict())
     db.add(db_profile)
     db.commit()
@@ -15,15 +14,15 @@ def post_profile(profile: ProfileSchema, db: Session = Depends(get_db)):
     return db_profile
 
 
-def get_profile(profile_id: int, db: Session = Depends(get_db)):
+def get_profile(profile_id: int, db: Session):
     return db.query(Profile).filter(Profile.id == profile_id).first()
 
 
-def get_profiles(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def get_profiles(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Profile).offset(skip).limit(limit).all()
 
 
-def delete_profile(profile_id: int, db: Session = Depends(get_db)):
-    db.query(Profile).filter(Profile.id==profile_id).delete()
+def delete_profile(profile_id: int, db: Session):
+    db.query(Profile).filter(Profile.id == profile_id).delete()
     db.commit()
     return
